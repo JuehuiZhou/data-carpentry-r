81ad597ed252c6f5a436344a973db6fb72069b80
Juehui Zhou <juehuizhou@Juehuis-MacBook-Air.local>
2019-07-25 15:38:19 +1000

    calculate values of area with different lenth and width.

---
+++ scripts/script 1.R
@@ -0,0 +1,18 @@
+#load required packages
+#install.packages("tidyverse")
+library(tidyverse)
+
+print("Hello, world")
+      
+area_hectares <- 1.0
+1.0 -> area_hectares
+
+area_hectares <- 2.5               # land area in hectares
+area_acres <- 2.47 * area_hectares # covert to acres
+area_acres                         #print
+
+lenth <- 5
+width <- 2
+area <- lenth * width
+area
+
