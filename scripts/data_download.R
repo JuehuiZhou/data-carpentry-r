f0a07c665cf4041aebf5da85ccb78d923e232826
Juehui Zhou <juehuizhou@Juehuis-MacBook-Air.local>
2019-07-26 15:19:28 +1000

    download Interviews_plotting.rds

--- scripts/data_download.R
+++ scripts/data_download.R
@@ -6,4 +6,5 @@ download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/
               
               download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_dates.xlsx", destfile = "./data/SAFI_dates.xlsx", mode = "wb") 
               
+              download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/Interviews_plotting.rds", destfile = "./data/Interviews_plotting.rds")              
               
